43b9ddc520c8155b51ec29e00b186e4f6e56b68f
chnng <526543674@qq.com>
2017-05-01 22:17:20 +0800

    tab

--- app/src/main/java/com/market/aching/ui/activity/MainActivity.java
+++ app/src/main/java/com/market/aching/ui/activity/MainActivity.java
@@ -1,26 +1,173 @@
 package com.market.aching.ui.activity;
 
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.MenuItem;
 
 import com.market.aching.R;
 import com.market.aching.ui.base.BaseActivity;
+import com.market.aching.ui.fragment.HomeFragment;
 
 import butterknife.BindView;
 
+import static com.market.aching.util.AchingUtil.log;
+
 public class MainActivity extends BaseActivity
 {
-    @BindView(R.id.toolbar)
-    Toolbar mToolbar;
+//    @BindView(R.id.toolbar)
+//    Toolbar mToolbar;
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+//    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    @BindView(R.id.container)
+    ViewPager mViewPager;
+
+    @BindView(R.id.navigation)
+    BottomNavigationView mNavigation;
+
     @Override
     protected int getLayoutId()
     {
-        return R.layout.activity_detail;
+        return R.layout.activity_main;
     }
 
     @Override
     protected void initViews(Bundle savedInstanceState)
     {
-        setSupportActionBar(mToolbar);
+//        setSupportActionBar(mToolbar);
+        mNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+//        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager.setAdapter(new SectionsPagerAdapter(getSupportFragmentManager()));
+
+    }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener()
+    {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item)
+        {
+            log(item.getOrder() + item.getGroupId());
+            switch (item.getItemId())
+            {
+                case R.id.navigation_home:
+                    mViewPager.setCurrentItem(0);
+                    return true;
+                case R.id.navigation_notifications:
+                    mViewPager.setCurrentItem(1);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mViewPager.setCurrentItem(2);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+//    public static class PlaceholderFragment extends Fragment
+//    {
+//        /**
+//         * The fragment argument representing the section number for this
+//         * fragment.
+//         */
+//        private static final String ARG_SECTION_NUMBER = "section_number";
+//
+//        public PlaceholderFragment()
+//        {
+//        }
+//
+//        /**
+//         * Returns a new instance of this fragment for the given section
+//         * number.
+//         */
+//        public static PlaceholderFragment newInstance(int sectionNumber)
+//        {
+//            PlaceholderFragment fragment = new PlaceholderFragment();
+//            Bundle args = new Bundle();
+//            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+//            fragment.setArguments(args);
+//            return fragment;
+//        }
+//
+//        @Override
+//        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                                 Bundle savedInstanceState)
+//        {
+//            View rootView = inflater.inflate(R.layout.fragment_tab, container, false);
+//            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+////            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+//            return rootView;
+//        }
+//    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    private class SectionsPagerAdapter extends FragmentPagerAdapter
+    {
+
+        private SectionsPagerAdapter(FragmentManager fm)
+        {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position)
+        {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return HomeFragment.getInstance(position);
+        }
+
+        @Override
+        public int getCount()
+        {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position)
+        {
+            switch (position)
+            {
+                case 0:
+                    return "SECTION 1";
+                case 1:
+                    return "SECTION 2";
+                case 2:
+                    return "SECTION 3";
+            }
+            return null;
+        }
     }
 }
--- app/src/main/java/com/market/aching/ui/base/BaseFragment.java
+++ app/src/main/java/com/market/aching/ui/base/BaseFragment.java
@@ -1,11 +1,49 @@
 package com.market.aching.ui.base;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.market.aching.R;
+
+import butterknife.ButterKnife;
 
 /**
  * Created by Administrator on 2016/11/5.
  */
 
-public class BaseFragment extends Fragment
+public abstract class BaseFragment extends Fragment
 {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        View rootView = inflater.inflate(getLayoutId(), container, false);
+        ButterKnife.bind(this, rootView);
+        final Toolbar toolbar = (Toolbar) rootView.findViewById(getToolBar());
+        ((BaseActivity) getActivity()).setSupportActionBar(toolbar);
+        initViews(savedInstanceState);
+        return rootView;
+    }
+
+    protected abstract int getToolBar();
+
+    protected abstract int getLayoutId();
+
+    protected abstract void initViews(Bundle savedInstanceState);
+
+    protected void setTitle(String title)
+    {
+        ActionBar actionBar = ((BaseActivity) getActivity()).getSupportActionBar();
+        if (null != actionBar)
+        {
+            actionBar.setTitle(title);
+        }
+    }
 }
